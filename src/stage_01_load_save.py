9b79ddb3c5d9600c177802f1889f8d369e0b9b67
Harry <subhadipde12@gmail.com>
2022-01-27 21:55:13 +0530

    added some stuff

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -6,6 +6,13 @@ import shutil
 from tqdm import tqdm
 import logging
 
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
+                    filemode="a")
+
+
 def copy_file(source_download_dir, local_data_dir):
     list_of_files = os.listdir(source_download_dir)
     N = len(list_of_files)
